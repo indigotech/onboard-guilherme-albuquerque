beef3abe5e4ef0702b994413e80fdaa0147d7ecb
Guilherme <guilherme.albuquerque@taqtile.com.br>
2020-09-03 09:05:23 -0300

    basic validations

--- adventure-beta/src/App.tsx
+++ adventure-beta/src/App.tsx
@@ -7,14 +7,18 @@ function App() {
       <h1>Bem vindo(a) à Taqtile!</h1>
       <form>
         <label> E-mail
-          <input type="email" required={true} pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"/>
+          <input
+            type="email" required={true} pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"/>
         </label>
 
         <label> Senha
-          <input type="password" required={true} pattern="(?=.*\d)(?=.*[a-z]).{7,}" title="No mínimo um número e uma letra, mais de 7 caracteres"/>
+          <input
+            type="password" required={true} pattern="(?=.*\d)(?=.*[a-z]).{7,}"
+            title=" A sua senha deve ter pelo menos 7 caracteres e conter pelo menos:
+            uma letra minúscula e um dígito. "/>
         </label>
 
-        <input type="submit" />
+        <input type="submit" value="Entrar"/>
       </form>
     </div>
   );
