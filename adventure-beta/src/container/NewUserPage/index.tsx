46fbabd4d8cc672fae0b60bcc3b6bd08240ca10a
Guilherme <guilherme.albuquerque@taqtile.com.br>
2020-09-09 18:06:30 -0300

    create forms with some validation and setState

--- adventure-beta/src/container/NewUserPage/index.tsx
+++ adventure-beta/src/container/NewUserPage/index.tsx
@@ -1,47 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
+import { FormContainer } from "./style";
 
 function NewUser() {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    birthDate: "",
+    password: "",
+    role: "",
+  });
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setState({
+      ...state,
+      [e.target.name]: value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(state);
+  };
   return (
     <div>
       <h1>Tela de criação de usuários</h1>
 
-      <form>
+      <FormContainer onSubmit={handleSubmit}>
         <label>
           {"Nome"}
-
-          <input />
+          <input
+            type="text"
+            name="name"
+            value={state.name}
+            required={true}
+            pattern="[a-zA-Záàâãéèêíïóôõöúçñ ]{1,36}"
+            onChange={handleInput}
+            title="Não é permitido números e caracteres especiais"
+          />
         </label>
 
         <label>
           {"Email"}
-
-          <input />
+          <input
+            type="text"
+            name="email"
+            value={state.email}
+            required={true}
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+            onChange={handleInput}
+          />
         </label>
 
         <label>
           {"Telefone"}
-
-          <input />
+          <input
+            type="tel"
+            name="phone"
+            pattern={"[0-9]{8,14}"}
+            value={state.phone}
+            required={true}
+            onChange={handleInput}
+          />
         </label>
 
         <label>
           {"Data de Nascimento"}
-
-          <input />
+          <input
+            placeholder="yyyy-mm-dd"
+            name="birthDate"
+            pattern="[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])"
+            title="Formato aaaa-mm-dd"
+            value={state.birthDate}
+            required={true}
+            onChange={handleInput}
+          />
         </label>
 
         <label>
           {"Senha"}
 
-          <input />
+          <input
+            type="password"
+            name="password"
+            pattern="[a-z0-9]{8,}"
+            title="No mínimo 8 caracteres"
+            value={state.password}
+            required={true}
+            onChange={handleInput}
+          />
         </label>
 
         <label>
           {"Papel no sistema"}
 
-          <input />
+          <input
+            type="text"
+            name="role"
+            value={state.role}
+            required={true}
+            onChange={handleInput}
+          />
         </label>
-      </form>
+
+        <input type="submit" value="Criar usuário" />
+      </FormContainer>
     </div>
   );
 }
---
+++ adventure-beta/src/container/NewUserPage/style.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const FormContainer = styled.form `
+  display: flex;
+  flex-direction: column;
+
+  label {
+    margin-top: 15px;
+  }
+
+  input {
+    margin-left: 10px;
+  }
+`
